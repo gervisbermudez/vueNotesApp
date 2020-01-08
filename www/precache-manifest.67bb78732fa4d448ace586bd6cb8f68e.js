21a210fce55d8fa3b3236230937eef8e79e2664d
Gervis Bermudez <gbermudezmora@quaresitsolutions.com>
2020-01-08 17:47:05 -0300

    Adding VueNote build files

---
+++ www/precache-manifest.67bb78732fa4d448ace586bd6cb8f68e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "26cc952fe4e7bbf47790",
+    "url": "js/chunk-3383a67e.0714d693.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "e64b9d54415d9f21832b",
+    "url": "css/chunk-45d54121.d324756d.css"
+  },
+  {
+    "revision": "8e3dcc7b44dbed9e0837",
+    "url": "css/chunk-13391fe4.9b50d9d5.css"
+  },
+  {
+    "revision": "e0c1a691334ba83e03d1",
+    "url": "css/chunk-2243e0cb.9b50d9d5.css"
+  },
+  {
+    "revision": "e0c1a691334ba83e03d1",
+    "url": "js/chunk-2243e0cb.66d12b25.js"
+  },
+  {
+    "revision": "26cc952fe4e7bbf47790",
+    "url": "css/chunk-3383a67e.d324756d.css"
+  },
+  {
+    "revision": "a9ee5ca49997db322778",
+    "url": "js/app.83dcac41.js"
+  },
+  {
+    "revision": "8e3dcc7b44dbed9e0837",
+    "url": "js/chunk-13391fe4.aa80097f.js"
+  },
+  {
+    "revision": "e64b9d54415d9f21832b",
+    "url": "js/chunk-45d54121.916c8e50.js"
+  },
+  {
+    "revision": "5221f9fb21610bf41a04",
+    "url": "css/chunk-vendors.d66885cc.css"
+  },
+  {
+    "revision": "5221f9fb21610bf41a04",
+    "url": "js/chunk-vendors.ed82a222.js"
+  },
+  {
+    "revision": "fc15915c446aa4c55ec24f590a18983e",
+    "url": "index.html"
+  },
+  {
+    "revision": "a9ee5ca49997db322778",
+    "url": "css/app.3b2cf980.css"
+  }
+];
